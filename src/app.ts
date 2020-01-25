b74aa844a18009557a7d2131ed9e810ff200c9a5
wiredmartian <11290243+wiredmartian@users.noreply.github.com>
2020-01-25 17:26:28 +0200

    2020 init commit

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+// Require the framework and instantiate it
+import fastify from 'fastify';
+
+
+const server = fastify({
+    logger: true,
+    caseSensitive: false,
+    ignoreTrailingSlash: true,
+});
+
+server.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+// Run the server!
+const start = async () => {
+  try {
+    await server.listen(3000)
+    server.log.info(`server listening on ${server.server.address()}`)
+  } catch (err) {
+    server.log.error(err)
+    process.exit(1)
+  }
+}
+start()
