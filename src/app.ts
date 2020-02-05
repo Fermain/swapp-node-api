d9e40414b8a360756e24e1247e2b3ae69b3306d6
wiredmartian <11290243+wiredmartian@users.noreply.github.com>
2020-02-05 17:33:16 +0200

    working on app middleware
    Signed-off-by: wiredmartian <meggatym@yahoo.com>

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,10 @@
 import fastify from 'fastify';
+import fastifyJWT from 'fastify-jwt';
+import helmet from 'fastify-helmet';
+import sensible from 'fastify-sensible';
+import rateLimit from 'fastify-rate-limit';
+import fastifyCORS from 'fastify-cors';
+
 import { authController } from './conrollers/auth.controller';
 
 const server = fastify({
@@ -13,6 +19,23 @@ const server = fastify({
 server.register(authController);
 /***/
 
+/** middleware */
+server.register(rateLimit, {
+    max: 100,
+    timeWindow: 2000,
+    whitelist: ['127.0.0.1', '127.0.0.1:8080'],
+});
+server.register(helmet, { hidePoweredBy: { setTo: 'Mytelnet' } });
+server.register(fastifyJWT, { secret: env.AUTH_JWT_PRIVATE_KEY });
+server.register(fastifyCORS, {
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+});
+server.register(sensible);
+/** middleware */
+
 const start = async () => {
   try {
     await server.listen(3000);
@@ -22,4 +45,10 @@ const start = async () => {
     process.exit(1);
   }
 };
+process.on('uncaughtException', err => {
+    console.error(err);
+});
+process.on('unhandledRejection', err => {
+    console.error(err);
+});
 start();
