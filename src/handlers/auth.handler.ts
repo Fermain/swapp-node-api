d9125bde5fe8db520a9b694216437abfa99bd894
wiredmartian <11290243+wiredmartian@users.noreply.github.com>
2020-02-05 19:09:29 +0200

    can generate auth token
    Signed-off-by: wiredmartian <meggatym@yahoo.com>

--- src/handlers/auth.handler.ts
+++ src/handlers/auth.handler.ts
@@ -1,29 +1,48 @@
 import {LoginModel} from "../models/account.models";
 import {FastifyReply, FastifyRequest} from "fastify";
 import {IncomingMessage, ServerResponse} from "http";
-import { sign } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+import envSchema from "env-schema";
+
+const schema = {
+    type: 'object',
+    required: ['JWT_SECRET_KEY'],
+    properties: {
+        JWT_SECRET_KEY: { type: 'string' }
+    }
+};
+const config = envSchema({
+    schema: schema,
+    dotenv: true
+});
 
 export class AuthHandler {
 
-    public async generateAuthToken(request: FastifyRequest<IncomingMessage>, reply: FastifyReply<ServerResponse>) {
+    public static async generateAuthToken(request: FastifyRequest<IncomingMessage>, reply: FastifyReply<ServerResponse>) {
         try {
-            const { username, password } = request.body as LoginModel;
-            const token = sign({
+            const { username, password } = request.body;
+            console.log(username, password);
+            const token = jwt.sign({
                 username: username,
-            }, '', {
-                algorithm: "ES512",
-                issuer: "swap api",
+            }, config.JWT_SECRET_KEY, {
+                issuer: "wiredmartian",
+                algorithm: "HS512",
                 expiresIn: "1h"
             });
-            reply.send(token);
+            reply.send({ token });
         } catch (e) {
+            console.log(e);
             reply.unauthorized(e.message);
         }
     }
     public static async authInterceptor(request: FastifyRequest<IncomingMessage>, reply: FastifyReply<ServerResponse>) {
-        const token = request.headers.authorization;
+        const authHeader = request.headers.authorization;
+        const anonymousRoutes = ['token'];
         try {
-            if (!token) {
+            if (request.req.url.includes(anonymousRoutes[0])) {
+                return;
+            }
+            if (!authHeader) {
                 reply.forbidden('Request missing auth token');
             }
             await request.jwtVerify();
