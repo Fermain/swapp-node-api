af4a0acee3e5a398749a134a691e3c12a9f0a511
wiredmartian <11290243+wiredmartian@users.noreply.github.com>
2020-02-05 18:04:37 +0200

    generate auth tokens
    Signed-off-by: wiredmartian <meggatym@yahoo.com>

--- src/handlers/auth.handler.ts
+++ src/handlers/auth.handler.ts
@@ -1,11 +1,24 @@
-import { LoginModel } from "../models/account.models";
-import { FastifyReply, FastifyRequest } from "fastify";
-import { IncomingMessage, ServerResponse } from "http";
+import {LoginModel} from "../models/account.models";
+import {FastifyReply, FastifyRequest} from "fastify";
+import {IncomingMessage, ServerResponse} from "http";
+import { sign } from "jsonwebtoken";
 
 export class AuthHandler {
 
-    public async generateAuthToken(model: LoginModel): Promise<string> {
-        return 'token';
+    public async generateAuthToken(request: FastifyRequest<IncomingMessage>, reply: FastifyReply<ServerResponse>) {
+        try {
+            const { username, password } = request.body as LoginModel;
+            const token = sign({
+                username: username,
+            }, '', {
+                algorithm: "ES512",
+                issuer: "swap api",
+                expiresIn: "1h"
+            });
+            reply.send(token);
+        } catch (e) {
+            reply.unauthorized(e.message);
+        }
     }
     public static async authInterceptor(request: FastifyRequest<IncomingMessage>, reply: FastifyReply<ServerResponse>) {
         const token = request.headers.authorization;
