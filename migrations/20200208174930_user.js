9f0531408bb9ec80fdfccb58493c75cf53223565
wiredmartian <11290243+wiredmartian@users.noreply.github.com>
2020-02-08 18:01:52 +0200

    users table
    Signed-off-by: wiredmartian <meggatym@yahoo.com>

---
+++ migrations/20200208174930_user.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('users', function (table) {
+        table.uuid('id').unsigned().primary();
+        table.string('full_name').notNullable();
+        table.string('email_address').unique().notNullable();
+        table.string('password').notNullable();
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('last_signed_in').nullable();
+    });
+};
+exports.down = function(knex) {
+    return knex.schema.dropTable('users');
+};
