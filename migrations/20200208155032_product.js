e9b786161eab10b395c6854ec09a6b0f5fa19028
wiredmartian <11290243+wiredmartian@users.noreply.github.com>
2020-02-08 16:43:33 +0200

    products schema
    Signed-off-by: wiredmartian <meggatym@yahoo.com>

---
+++ migrations/20200208155032_product.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('products', function (table) {
+    table.increments('id').unsigned().primary();
+    table.string('name', 255).notNullable();
+    table.text('description', 'mediumtext').notNullable();
+    table.string('location', 255).notNullable();
+    table.string('ideal_exchange').nullable();
+    table.boolean('is_available').defaultTo(true);
+    table.boolean('is_free').defaultTo(false);
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.boolean('cancelled').defaultTo(false);
+    /** reference */
+    table.foreign('category_id').references('categories.id');
+  });
+};
+exports.down = function(knex) {
+  return knex.schema.dropTable('products');
+};
