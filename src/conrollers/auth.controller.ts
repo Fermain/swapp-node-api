a76d9eb7da706359da7c2f14791991b2b08f1795
wiredmartian <11290243+wiredmartian@users.noreply.github.com>
2020-02-13 21:21:57 +0200

    response from creating a user
    Signed-off-by: wiredmartian <meggatym@yahoo.com>

--- src/conrollers/auth.controller.ts
+++ src/conrollers/auth.controller.ts
@@ -3,16 +3,24 @@ import { FastifyInstance } from "fastify";
 import { AuthHandler } from "../handlers/auth.handler";
 import { authSchema } from "../schemas/auth.schema";
 import {UserHandler} from "../handlers/user.handler";
+import {ICreateUser} from "../models/account.models";
 export const authController = fastifyPlugin(async (server:FastifyInstance, options: PluginOptions, next: nextCallback) => {
     server.route({
         method: "POST",
         url: "/register",
         schema: authSchema.registerUser.schema,
-        preHandler: (request, reply) => {
-
-        },
-        handler: (request, reply) => {
-
+        handler: async (request, reply) => {
+            const user = {...request.body} as ICreateUser;
+            try {
+                const res = await UserHandler.createUser(user);
+                if (res) {
+                    return reply.send({ success: true, message: 'user created!'});
+                }
+                /** needs an appropriate message here */
+                return reply.send({ success: false, message: 'Failed to create a user'});
+            } catch (e) {
+                return reply.badRequest(e);
+            }
         }
     });
     server.route({
