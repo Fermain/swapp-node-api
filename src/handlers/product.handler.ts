f3411125c43b2bb61a3afc51bff16a2b8912fc82
wiredmartian <meggatym@yahoo.com>
2020-02-23 22:30:49 +0200

    save product and its images on one function using a transaction
    Signed-off-by: wiredmartian <meggatym@yahoo.com>

--- src/handlers/product.handler.ts
+++ src/handlers/product.handler.ts
@@ -18,16 +18,38 @@ export class ProductHandler {
     }
 
     public async addProduct(profileId: number, product: Product, images: IMulterFile[]) {
-
-    }
-
-    private async storeProductImages(productId: number, images: IMulterFile[]) {
-        const imageFields = images.map((img: IMulterFile) => {
-            return {
-                image_path: img.path,
-                product_id: productId
+        const trx = await SWAPCONNECTION.transaction();
+        try {
+            const _product: any[] = await trx<Product>('products')
+                .insert({
+                    name: product.name,
+                    description: product.description,
+                    location: product.location,
+                    ideal_exchange: product.ideal_exchange,
+                    is_available: product.is_available,
+                    is_free: product.is_free,
+                    cancelled: false
+                }, ['*']);
+            if (_product.length > 0) {
+                const productId: number = _product[0].id;
+                const imageFields = images.map((img: IMulterFile) => {
+                    return {
+                        image_path: img.path,
+                        product_id: productId
+                    }
+                });
+                const _images = await trx('product_images')
+                    .insert(imageFields, ['*']);
+                if (_images.length > 0) {
+                    await trx.commit();
+                    return _product;
+                }
+                return new Error('Failed to save product images');
             }
-        });
-        return SWAPCONNECTION.insert(imageFields, ['*']);
+            return new Error('Failed to save product');
+        } catch (e) {
+            await trx.rollback(e);
+            return new Error(e);
+        }
     }
 }
