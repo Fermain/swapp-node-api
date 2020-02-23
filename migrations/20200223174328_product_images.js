3c847ccf7e7ab8774c08133515f80f432979ba98
wiredmartian <11290243+wiredmartian@users.noreply.github.com>
2020-02-23 18:58:48 +0200

    store images on folder on ref on the db
    Signed-off-by: wiredmartian <meggatym@yahoo.com>

--- migrations/20200223174328_product_images.js
+++ migrations/20200223174328_product_images.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('product_images', function (table) {
+    table.uuid('id').unsigned().primary();
+    table.string('image_path').notNullable();
+    table.integer('product_id')
+        .references('id').inTable('products');
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('product_images');
 };
